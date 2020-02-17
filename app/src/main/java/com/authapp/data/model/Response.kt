18c2aeec9e704e64ab10b41b3dea0e83b7706833
Jagruti <work.jagruti@gmail.com>
2020-02-17 22:26:50 +0530

    Auth App code modifications

--- app/src/main/java/com/authapp/data/model/Response.kt
+++ app/src/main/java/com/authapp/data/model/Response.kt
@@ -1,3 +1,3 @@
 package com.authapp.data.model
 
-data class Response<out T>(val data: T?, val success:Boolean, val errMsg:String?)
+data class Response<T>(val data: T?, val success:Boolean, val errMsg:String?)
